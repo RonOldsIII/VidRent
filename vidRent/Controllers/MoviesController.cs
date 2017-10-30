25a252df8070bd46ddd71a14bb87d82631944cdd
Ron Olds <rmoiii@gmail.com>
2017-10-30 15:37:52 -0400

    update

---
+++ vidRent/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using vidRent.Models;
+using vidRent.ViewModels;
+
+namespace vidRent.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "cust 1"},
+                new Customer {Name = "cust 2"},
+                new Customer {Name = "cust 3"},
+                new Customer {Name = "cust 4"},
+                new Customer {Name = "cust 5"},
+                new Customer {Name = "cust 6"}
+
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+        return View(viewModel);
+        }
+    }
+}
