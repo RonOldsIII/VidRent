2cf37394ad47cd6ef4d16d1c1f94ef6b3fceaf2b
Ron Olds <rmoiii@gmail.com>
2017-11-03 14:48:28 -0400

    DB configuration

--- vidRent/Models/Customer.cs
+++ vidRent/Models/Customer.cs
@@ -8,6 +8,12 @@ namespace vidRent.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }   
+        public string Name { get; set; }
+        public bool isSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public int MembershipTypeId { get; set; }
+
+
+
     }
 }
